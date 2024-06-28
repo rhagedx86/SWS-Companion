4d3c35d109beb938b4482f4d4a0fd3b40f594d1f
rhagedx86 <144078658+rhagedx86@users.noreply.github.com>
2024-06-28 10:40:35 -0600

    Update load.py
    Fixed bug, Added url for api endpoint.

--- SwS-Companion/load.py
+++ SwS-Companion/load.py
@@ -9,10 +9,10 @@ from edmc_data import commodity_bracketmap as bracketmap
 from typing import Optional
 from companion import CAPIData, SERVER_LIVE, SERVER_LEGACY, SERVER_BETA
 
-# This could also be returned from plugin_start3()
 plugin_name = os.path.basename(os.path.dirname(__file__))
-
 logger = logging.getLogger(f'{appname}.{plugin_name}')
+
+# Start logging
 if not logger.hasHandlers():
     level = logging.INFO
     logger.setLevel(level)
@@ -23,23 +23,24 @@ if not logger.hasHandlers():
     logger_channel.setFormatter(logger_formatter)
     logger.addHandler(logger_channel)
 
-
+# Listen to EDMC trasmit function. 
 def cmdr_data(data, is_beta):
     if data.get('commander') is None or data['commander'].get('name') is None:
         raise ValueError("Commander name is missing")
-
+        
     # Determining source galaxy for the data
     if data.source_host == SERVER_LIVE:
         control = data['lastStarport']['minorfaction']
         if control == "Sidewinder Syndicate":
             # Extracting the file name from the lastStarport data
             name = data['lastSystem']['name']
+            # This data is ONLY station market info!
             data['lastStarport']['system'] = name
 
             # Send data['lastStarport'] directly to the API
-            # This data is ONLY station market info!
             send_data_to_api(data['lastStarport'])
-            
+
+# Send market data to database, just how Inara handles it.
 def send_data_to_api(json_data):
     api_url = "http://sidewindersyndicate.site/api/data"
     try:
@@ -54,11 +55,12 @@ def send_data_to_api(json_data):
     except Exception as e:
         logger.error(f"Error sending data to API: {str(e)}")
         
-        
+# Startup
 def plugin_start3(plugin_dir: str) -> str:
     logger.info('SWS-Companion-loaded')
     return "SWS-Companion"
 
+# Info
 def plugin_prefs(parent: nb.Notebook, cmdr: str, is_beta: bool) -> Optional[tk.Frame]:
    frame = nb.Frame(parent)
    nb.Label(frame, text="Hello").grid()
