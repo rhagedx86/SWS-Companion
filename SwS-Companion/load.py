adf539c4b9b419a63a3598a3d4a2c8ed52a1afd2
rhagedx86 <144078658+rhagedx86@users.noreply.github.com>
2024-06-28 10:34:03 -0600

    Update load.py

--- SwS-Companion/load.py
+++ SwS-Companion/load.py
@@ -7,6 +7,7 @@ import myNotebook as nb
 from config import appname
 from edmc_data import commodity_bracketmap as bracketmap
 from typing import Optional
+from companion import CAPIData, SERVER_LIVE, SERVER_LEGACY, SERVER_BETA
 
 # This could also be returned from plugin_start3()
 plugin_name = os.path.basename(os.path.dirname(__file__))
@@ -22,40 +23,25 @@ if not logger.hasHandlers():
     logger_channel.setFormatter(logger_formatter)
     logger.addHandler(logger_channel)
 
-from companion import CAPIData, SERVER_LIVE, SERVER_LEGACY, SERVER_BETA
-
-folder_path = os.path.expanduser("~/Documents/sws")
-os.makedirs(folder_path, exist_ok=True)
 
 def cmdr_data(data, is_beta):
     if data.get('commander') is None or data['commander'].get('name') is None:
         raise ValueError("Commander name is missing")
-
-    logger.info(data['commander']['name'])
 
     # Determining source galaxy for the data
     if data.source_host == SERVER_LIVE:
         control = data['lastStarport']['minorfaction']
         if control == "Sidewinder Syndicate":
             # Extracting the file name from the lastStarport data
-            file_name = f"{data['lastStarport']['name'].replace(' ', '_')}_data.json"
-            file_path = os.path.join(folder_path, file_name)
             name = data['lastSystem']['name']
             data['lastStarport']['system'] = name
-            logger.info(data['lastStarport']['system'])
-            
-            # Saving the lastStarport data to the file
-            with open(file_path, 'w') as file:
-                json.dump(data['lastStarport'], file)
-            
-            logger.info(f"Data saved to {file_path}")
-            
+
             # Send data['lastStarport'] directly to the API
+            # This data is ONLY station market info!
             send_data_to_api(data['lastStarport'])
-
-
+            
 def send_data_to_api(json_data):
-    api_url = "http://15.204.67.28:80/api/data"  # Replace with your API endpoint URL
+    api_url = "http://sidewindersyndicate.site/api/data"
     try:
         headers = {'Content-Type': 'application/json'}
         response = requests.post(api_url, json=json_data, headers=headers)
@@ -70,7 +56,7 @@ def send_data_to_api(json_data):
         
         
 def plugin_start3(plugin_dir: str) -> str:
-    logger.info('SWS-EDMC-loaded')
+    logger.info('SWS-Companion-loaded')
     return "SWS-Companion"
 
 def plugin_prefs(parent: nb.Notebook, cmdr: str, is_beta: bool) -> Optional[tk.Frame]:
